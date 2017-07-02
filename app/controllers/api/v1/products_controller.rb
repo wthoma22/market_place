b38e3e2fd0c225c423dae753f10e4e7a177b211a
William Thomas <wthoma390@gmail.com>
2017-07-01 19:03:26 -0600

    create and pass products controller for POST

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
+  before_action :authenticate_with_token!, only: [:create]
   respond_to :json
 
   def index
@@ -7,5 +8,20 @@ class Api::V1::ProductsController < ApplicationController
 
   def show
     respond_with Product.find(params[:id])
+  end
+
+  def create
+    product = current_user.products.build(product_params)
+    if product.save
+      render json: product, status: 201, location: [:api, product]
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(:title, :price, :published)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ MarketPlaceApi::Application.routes.draw do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, :only => [:show, :create, :update, :destroy]
+      resources :users, :only => [:show, :create, :update, :destroy] do
+        resources :products, :only => [:create]
+      end
       resources :sessions, :only => [:create, :destroy]
       resources :products, :only => [:show, :index]
     end
