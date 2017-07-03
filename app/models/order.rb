7cc72dee6d14c21b95e184455657a1d637abcd01
William Thomas <wthoma390@gmail.com>
2017-07-03 12:44:00 -0600

    Associates products and orders with a placements model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ActiveRecord::Base
   belongs_to :user
+  has_many :placements
+  has_many :products, through: :placements
 
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
-
   validates :user_id, presence: true
 end
